bc5fa35207ba655a576c6298e1ed0f6b7f51b0b5
nouhaila <nouhailabenja09@gmail.com>
2023-04-05 16:15:39 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,3 +1,6 @@
+es (26 sloc)  427 Bytes
+ 
+
 #include "main.h"
 
 /**
@@ -14,4 +17,16 @@ int check(int i, int j)
 	else if (i * i > j)
 		return (-1);
 	return (check(i + 1, j));
+}
+/**
+ * _sqrt_recursion - return the natural square root of a number
+ * @n: number
+ * Return: natural square of num
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n == 0)
+		return (0);
+	return (check(1, n));
 }
